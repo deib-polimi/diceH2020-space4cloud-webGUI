b8b79f97cedb3e66c17192d16c11ea43f7a171c4
miciav <michele.ciavotta@gmail.com>
2016-02-17 12:21:54 +0100

    Restructured

--- Laucher/src/main/java/it/polimi/diceH2020/launcher/Experiment.java
+++ Laucher/src/main/java/it/polimi/diceH2020/launcher/Experiment.java
@@ -2,14 +2,16 @@ package it.polimi.diceH2020.launcher;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileAttribute;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.xbean.blueprint.context.impl.FileEditor;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -26,6 +29,7 @@ import it.polimi.diceH2020.SPACE4Cloud.shared.solution.Solution;
 
 @Service
 public class Experiment {
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
 
 	private ObjectMapper mapper;
 	@Autowired
@@ -35,26 +39,41 @@ public class Experiment {
 	private static String EVENT_ENDPOINT;
 	private static String STATE_ENDPOINT;
 	private static String UPLOAD_ENDPOINT;
-	private static Path LOCAL_DYNAMIC_FOLDER;
+	private static String RESULT_FOLDER;
 
-	public Experiment() throws IOException {
+	public Experiment() {
 		mapper = new ObjectMapper();
 		SimpleModule module = new SimpleModule();
 		module.addKeyDeserializer(TypeVMJobClassKey.class, TypeVMJobClassKey.getDeserializer());
 		mapper.registerModule(module);
-		LOCAL_DYNAMIC_FOLDER = Paths.get(settings.getResultDir());
-		Files.deleteIfExists(LOCAL_DYNAMIC_FOLDER);
-		Files.createDirectory(LOCAL_DYNAMIC_FOLDER);
-
 	}
 
 	@PostConstruct
-	private void init() {
+	private void init() throws IOException {
 		INPUTDATA_ENDPOINT = settings.getfullAddress() + "/inputdata";
 		EVENT_ENDPOINT = settings.getfullAddress() + "/event";
 		STATE_ENDPOINT = settings.getfullAddress() + "/state";
 		UPLOAD_ENDPOINT = settings.getfullAddress() + "/upload";
 		SOLUTION_ENDPOINT = settings.getfullAddress() + "/solution";
+		Path result = Paths.get(settings.getResultDir());
+		Files.exists(result);
+		// Files.deleteIfExists(result);
+		Files.walkFileTree(result, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Files.delete(file);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				Files.delete(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
+		Files.deleteIfExists(result);
+		Files.createDirectory(result);
+		RESULT_FOLDER = result.toAbsolutePath().toString();
 
 	}
 
@@ -62,58 +81,151 @@ public class Experiment {
 
 	public void launch(Path inputDataPath) {
 
+		String baseErrorString = "Error for experiment: " + inputDataPath.getName(inputDataPath.getNameCount() - 1);
+
+		boolean idle = checkWSIdle();
+
+		if (!idle) {
+			logger.info(baseErrorString + "-> service not idle");
+			return;
+		}
+
+		boolean charged_inputdata = sendInputData(inputDataPath);
+
+		if (!charged_inputdata) return;
+
+		boolean charged_initsolution = generateInitialSolution();
+
+		if (!charged_initsolution) {
+			logger.info(baseErrorString + "-> generation of the initial solution");
+			return;
+		}
+
+		boolean initsolution_saved = saveInitSolution();
+		if (!initsolution_saved) {
+			logger.info(baseErrorString + "-> getting or saving initial solution");
+			return;
+		}
+
+		boolean finish = executeLocalSearch();
+
+		if (!finish) {
+			logger.info(baseErrorString + "-> local search");
+			return;
+		}
+
+		boolean finalSolution_saved = saveFinalSolution();
+		if (!finalSolution_saved) {
+			logger.info(baseErrorString + "-> getting or saving final solution");
+			return;
+		}
+
+	}
+
+	private boolean saveFinalSolution() {
+		Solution sol = restTemplate.getForObject(SOLUTION_ENDPOINT, Solution.class);
+		String solFilePath = RESULT_FOLDER + File.separator + sol.getId() + "-final.json";
+		String solSerialized;
+		try {
+			solSerialized = mapper.writeValueAsString(sol);
+			Files.write(Paths.get(solFilePath), solSerialized.getBytes());
+			logger.info(sol.toStringReduced());
+			return true;
+		} catch (JsonProcessingException e) {
+			return false;
+		}
+		// System.out.println(serialized);
+		catch (IOException e) {
+			return false;
+		}
+	}
+
+	private boolean executeLocalSearch() {
+		String res = restTemplate.postForObject(EVENT_ENDPOINT, Events.TO_RUNNING_LS, String.class);
+		if (res.equals("RUNNING_LS")) {
+			res = "RUNNING_LS";
+			while (res.equals("RUNNING_LS")) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				res = restTemplate.getForObject(STATE_ENDPOINT, String.class);
+			}
+		}
+		if (res.equals("FINISH")) return true;
+		else return false;
+	}
+
+	private boolean saveInitSolution() {
+		Solution sol = restTemplate.getForObject(SOLUTION_ENDPOINT, Solution.class);
+		String solFilePath = RESULT_FOLDER + File.separator + sol.getId() + "-MINLP.json";
+		String solSerialized;
+		try {
+			solSerialized = mapper.writeValueAsString(sol);
+			Files.write(Paths.get(solFilePath), solSerialized.getBytes());
+			return true;
+		} catch (JsonProcessingException e) {
+			return false;
+		}
+		// System.out.println(serialized);
+		catch (IOException e) {
+			return false;
+		}
+
+	}
+
+	private boolean generateInitialSolution() {
+		String res = restTemplate.postForObject(EVENT_ENDPOINT, Events.TO_RUNNING_INIT, String.class);
+		if (res.equals("RUNNING_INIT")) {
+			res = "RUNNING_INIT";
+			while (res.equals("RUNNING_INIT")) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				res = restTemplate.getForObject(STATE_ENDPOINT, String.class);
+			}
+			if (res.equals("CHARGED_INITSOLUTION")) {
+				return true;
+			} else return false;
+		} else return false;
+	}
+
+	private boolean sendInputData(Path inputDataPath) {
+		InstanceData data = getObjectFromPath(inputDataPath);
+
+		if (data != null) {
+			String res = restTemplate.postForObject(INPUTDATA_ENDPOINT, data, String.class);
+			if (res.equals("CHARGED_INPUTDATA")) return true;
+			else {
+				logger.info("Error for experiment: " + inputDataPath.getName(inputDataPath.getNameCount() - 1) + " server respondend in an unexpected way: " + res);
+				return false;
+			}
+
+		} else {
+			logger.info("Error for experiment: " + inputDataPath.getName(inputDataPath.getNameCount() - 1) + " problem in inputdata serialization");
+			return false;
+		}
+	}
+
+	private InstanceData getObjectFromPath(Path inputDataPath) {
 		String serialized;
 		try {
 			serialized = new String(Files.readAllBytes(inputDataPath));
 			InstanceData data = mapper.readValue(serialized, InstanceData.class);
-			String res = restTemplate.postForObject(INPUTDATA_ENDPOINT, data, String.class);
-			if (res.equals("CHARGED_INPUTDATA")) {
-
-				res = restTemplate.postForObject(EVENT_ENDPOINT, Events.TO_RUNNING_INIT, String.class);
-				if (res.equals("RUNNING_INIT")) {
-					res = "RUNNING_INIT";
-					while (res.equals("RUNNING_INIT")) {
-						try {
-							Thread.sleep(2000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						res = restTemplate.getForObject(STATE_ENDPOINT, String.class);
-					}
-					if (res.equals("CHARGED_INITSOLUTION")) {
-						res = restTemplate.postForObject(EVENT_ENDPOINT, Events.TO_RUNNING_LS, String.class);
-						if (res.equals("RUNNING_INIT")) {
-							res = "RUNNING_LS";
-							while (res.equals("RUNNING_LS")) {
-								try {
-									Thread.sleep(2000);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-								res = restTemplate.getForObject(STATE_ENDPOINT, String.class);
-							}
-
-						}
-						if (res.equals("FINISH")) {
-							res = restTemplate.postForObject(EVENT_ENDPOINT, Events.RESET, String.class);
-							if (!res.equals("IDLE")) {
-								System.out.println("ERROR!!");
-							}
-
-							Solution sol = restTemplate.getForObject(SOLUTION_ENDPOINT, Solution.class);
-							String solSerialized = mapper.writeValueAsString(sol);
-							System.out.println(serialized);
-							String solFilePath = LOCAL_DYNAMIC_FOLDER+File.separator+sol.getId()+".json";
-							Files.write(Paths.get(solFilePath), solSerialized.getBytes());
-						}
-					}
-
-				}
-			}
-
+			return data;
 		} catch (IOException e) {
-			e.printStackTrace(); 	
+			return null;
 		}
+
+	}
+
+	private boolean checkWSIdle() {
+		String res = restTemplate.getForObject(STATE_ENDPOINT, String.class);
+		if (res.equals("IDLE")) return true;
+		else return false;
 
 	}
 
