b9f40a3e42cb89294f568456e751a626a9b60a6f
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-04-21 11:28:34 +0200

    Log errors with multipart requests and calibrate them

--- src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
+++ src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
@@ -1,4 +1,6 @@
 package it.polimi.diceH2020.launcher.controller;
+
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
@@ -6,11 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class UploadExceptionHandler {
+
+	private final Logger logger = Logger.getLogger(getClass());
+
 	@ExceptionHandler(MultipartException.class)
-	  public ModelAndView handleError(MultipartException exception) {
-	    ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.addObject("message", exception.getMessage());
-	    modelAndView.setViewName("error");
-	    return modelAndView;
-	  }
+	public ModelAndView handleError(MultipartException exception) {
+		logger.error("error loading multipart request", exception);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("message", exception.getMessage());
+		modelAndView.setViewName("error");
+		return modelAndView;
+	}
 }
