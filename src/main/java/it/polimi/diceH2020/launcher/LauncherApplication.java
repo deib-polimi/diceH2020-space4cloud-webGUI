025a110c767b20bfa01917fa141f8786e897cb10
jacoporigoli <jackeape@gmail.com>
2016-04-21 18:23:02 +0200

    MultifileUpload Set Tomcat maxSwallowSize

--- src/main/java/it/polimi/diceH2020/launcher/LauncherApplication.java
+++ src/main/java/it/polimi/diceH2020/launcher/LauncherApplication.java
@@ -1,12 +1,15 @@
 package it.polimi.diceH2020.launcher;
 
 import it.polimi.diceH2020.launcher.utility.FileUtility;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.apache.log4j.Logger;
 import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -32,28 +35,41 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @DependsOn("EmailSender")
 public class LauncherApplication {
 
-    private static Logger logger = Logger.getLogger(LauncherApplication.class.getName());
+	private static final int uploadTomcatMaxSize = -1; //for a correct redirect disable tomcat. set -1 to disable preupload check
+	private static Logger logger = Logger.getLogger(LauncherApplication.class.getName());
 
-    @Autowired
-    private FileUtility fileUtility;
+	@Autowired
+	private FileUtility fileUtility;
 
-    @EventListener
-    public void handleContextRefresh(ContextRefreshedEvent event) throws Exception {
-        try {
-            fileUtility.createWorkingDir();
-        } catch (Exception e) {
-            logger.info("Error in the creation of local work directory!");
-        }
-    }
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) throws Exception {
+		try {
+			fileUtility.createWorkingDir();
+		} catch (Exception e) {
+			logger.info("Error in the creation of local work directory!");
+		}
+	}
 
-    @Bean
-    public ServletRegistrationBean h2servletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-        registration.addUrlMappings("/console/*");
-        return registration;
-    }
+	@Bean
+	public TomcatEmbeddedServletContainerFactory containerFactory() {
+		return new TomcatEmbeddedServletContainerFactory() {
+			protected void customizeConnector(Connector connector) {
+				super.customizeConnector(connector);
+				if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+					((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(uploadTomcatMaxSize);
+				}
+			}
+		};
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(LauncherApplication.class, args);
-    }
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauncherApplication.class, args);
+	}
 }
--- src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
+++ src/main/java/it/polimi/diceH2020/launcher/controller/UploadExceptionHandler.java
@@ -1,10 +1,10 @@
 package it.polimi.diceH2020.launcher.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
-import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class UploadExceptionHandler {
@@ -12,11 +12,9 @@ public class UploadExceptionHandler {
 	private final Logger logger = Logger.getLogger(getClass());
 
 	@ExceptionHandler(MultipartException.class)
-	public ModelAndView handleError(MultipartException exception) {
-		logger.error("error loading multipart request", exception);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("message", exception.getMessage());
-		modelAndView.setViewName("error");
-		return modelAndView;
+	public String handleError(MultipartException exception, Model modelAndView) {
+		logger.error("error in multipart upload", exception);
+		modelAndView.addAttribute("message", exception);
+		return "error";
 	}
 }
