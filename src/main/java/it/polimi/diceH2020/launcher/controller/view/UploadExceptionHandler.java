93365844cbcfa6d77e1e5857092f25ea744f059a
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2017-02-14 11:21:39 +0100

    Clean up UploadExceptionHandler

--- src/main/java/it/polimi/diceH2020/launcher/controller/view/UploadExceptionHandler.java
+++ src/main/java/it/polimi/diceH2020/launcher/controller/view/UploadExceptionHandler.java
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package it.polimi.diceH2020.launcher.controller.view;
 
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,29 +23,14 @@ import org.springframework.web.multipart.MultipartException;
 
 @ControllerAdvice
 public class UploadExceptionHandler {
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UploadExceptionHandler.class.getName());
 
-//	@ExceptionHandler(MaxUploadSizeExceededException.class)
-//    public ModelAndView handleMaxUploadSizeExceededException(){
-//    	logger.info("Max file size exceeded");
-//		ModelAndView modelAndView = new ModelAndView();
-//		modelAndView.addObject("message", "Max file size exceeded");
-//		modelAndView.setViewName("error");
-//		return modelAndView;
-//    }
+    private final Logger logger = Logger.getLogger (getClass ());
 
-	@ExceptionHandler(MultipartException.class)
-	public String handleError(MultipartException exception,Model modelAndView) {
-		logger.info("MultipartUploadException: "+ exception);
-		modelAndView.addAttribute("message", exception);
-		return "error";
-	}
+    @ExceptionHandler(MultipartException.class)
+    public String handleError(MultipartException exception,Model modelAndView) {
+        logger.info("MultipartUploadException: "+ exception);
+        modelAndView.addAttribute("message", exception);
+        return "error";
+    }
 
-//	@ExceptionHandler(value = Exception.class)
-//    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception exception) throws Exception {
-//		ModelAndView modelAndView = new ModelAndView();
-//	    modelAndView.addObject("message", exception.getMessage());
-//	    modelAndView.setViewName("error");
-//        return modelAndView;
-//    }
 }
